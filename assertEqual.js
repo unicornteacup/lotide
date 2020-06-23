bada13e5d8f16e6107ebc06ca0f144d79f56e4b2
Caroline <carolinejepps@gmail.com>
2020-06-23 04:48:41 +0000

    assertEqual task

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(✅✅✅"Assertation Passed: " + actual + " === " + expected);
+  } else {
+    console.log(🛑🛑🛑"Assertation Failed: " + actual + " !== "+ expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(4,7);
+assertEqual("Cake", "Cake")
