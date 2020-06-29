b0d1a3ed7a56243c53f99c621279fc9308b5acaf
Caroline <carolinejepps@gmail.com>
2020-06-29 19:15:52 +0000

    Added eqArraysTest file

---
+++ test/eqArraysTest.js
@@ -0,0 +1,8 @@
+const eqArrays = require('../eqArrays')
+
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
